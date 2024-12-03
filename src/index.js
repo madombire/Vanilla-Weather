06e18ebf45ef118a7898e261eed33646973d9649
Mavis Atayo Adombire <145336116+madombire@users.noreply.github.com>
2024-12-03 15:07:17 +0100

    Completed work on the weather API integration

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
+function refreshWeather(response) {
+  console.log(response.data.temperature.current);
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+function searchCity(city) {
+  let apiKey = "4a2o4dfc38a1eb57a0teddf3b6b7f240";
+  let apiUrl = `
+  https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric
+`;
+  axios.get(apiUrl).then(refreshWeather);
+}
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input-form");
   console.log(searchInput.value);
-
-  let cityElement = document.querySelector("#city");
-  cityElement.textContent = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Accra");

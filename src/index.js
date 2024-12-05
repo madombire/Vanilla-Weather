ba30c58561d6c23439e8e50154f3ae461dc95808
Mavis Atayo Adombire <145336116+madombire@users.noreply.github.com>
2024-12-05 17:01:00 +0100

    Completed the data on weather conditions

--- src/index.js
+++ src/index.js
@@ -1,32 +1,64 @@
 function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
-  let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
   let humidityElement = document.querySelector("#humidity");
   let descriptionElement = document.querySelector("#weather-description");
   let windElement = document.querySelector("#wind-speed");
-  cityElement.innerHTML = response.data.city;
+  let timeElement = document.querySelector("#day-and-time");
+  let temperature = response.data.temperature.current;
+  let city = response.data.city;
+  let humidity = response.data.temperature.humidity;
+  let description = response.data.condition.description;
+  let windSpeed = response.data.wind.speed;
+  let timestamp = response.data.time * 1000;
+  let date = new Date(timestamp);
+  cityElement.innerHTML = city;
   temperatureElement.innerHTML = Math.round(temperature);
-  humidityElement.innerHTML = `${response.data.temperature.humidity} %,`;
-  descriptionElement.innerHTML = `${response.data.condition.description}`;
-  windElement.innerHTML = `${response.data.wind.speed}km/h`;
-  console.log(response.data);
+  humidityElement.innerHTML = `${humidity}%`;
+  descriptionElement.innerHTML = description;
+  windElement.innerHTML = `${Math.round(windSpeed)} km/h`;
+  timeElement.innerHTML = formatDate(date);
 }
+
+function formatDate(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}, `;
+}
+
 function searchCity(city) {
   let apiKey = "4a2o4dfc38a1eb57a0teddf3b6b7f240";
-  let apiUrl = `
-  https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric
-`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(refreshWeather);
 }
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input-form");
-  console.log(searchInput.value);
-  searchCity(searchInput.value);
+  if (searchInput.value.trim()) {
+    searchCity(searchInput.value);
+  } else {
+    alert("Please enter a valid city name!");
+  }
 }
 
+// Attach event listener to form
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
+// Initial call to display weather for "Accra"
 searchCity("Accra");
